1fc01fd422904b552472e9a9330e66b752084b9a
MUKILTEO\1717586 <1717586@mukilteo.wednet.edu>
2024-03-27 13:39:40 -0700

    ani afain

---
+++ PM12/Assets/Aliah/AutoMoveAni.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class AutoMoveAni : MonoBehaviour
+{
+    [SerializeField] float nuMoveSpeed = 3f;
+
+    Rigidbody2D nuRigidbody;
+    BoxCollider2D nuBoxCollider;
+
+    void Start()
+    {
+        nuRigidbody = GetComponent<Rigidbody2D>();
+        nuBoxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NuFacingRight())
+        {
+            nuRigidbody.velocity = new Vector2(nuMoveSpeed, nuRigidbody.velocity.y);
+            GetComponent<Animator>().SetBool("IsFaceRight", true);
+        }
+        else
+        {
+            nuRigidbody.velocity = new Vector2(-nuMoveSpeed, nuRigidbody.velocity.y);
+            GetComponent<Animator>().SetBool("IsFaceRight", false);
+        }
+    }
+    private bool NuFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Swap")
+        {
+            transform.localScale = new Vector2(-(Mathf.Sign(nuRigidbody.velocity.x)), transform.localScale.y);
+        }
+    }
+}
